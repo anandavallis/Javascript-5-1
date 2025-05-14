7193977f49543c0e831c6876d75655ce60bf1578
anandavallis <sanandavalli7@gmail.com>
2025-05-14 21:23:30 +0530

    Create isArray.js

---
+++ isArray.js
@@ -0,0 +1,11 @@
+
+    <h2>Array.isArray</h2>
+
+        function isArray(variable){
+            return Array.isArray(variable);
+        }
+
+        console.log(isArray([1,2,3]));
+        console.log(isArray('Hello'));
+        
+  
